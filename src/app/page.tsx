5410c6e1906664e313396b0bee36940c8ea05759
Naman Soni <namantsoni@gmail.com>
2025-01-22 12:34:00 +0530

    Documents Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
-import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 const Home = () => {
   return (
-    <div className="flex flex-col min-h-screen items-center justify-center">
-      <p>Hello World!!</p>
-      <Button>Click Me!</Button>
+    <div className="flex min-h-screen items-center justify-center">
+      Click <Link href="/documents/123"><span className="text-blue-500 underline">&nbsp;here&nbsp;</span></Link> to go to document id.
     </div>
   )
 }
